fe7af4f55201a3d02457e302e7a161f2d3f9a856
Anwar Jamis <anwarjamis@gmail.com>
2022-05-06 14:06:31 -0400

    db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Deleting all movies'
+Movie.destroy_all
+puts 'All movies destroyed'
+puts 'Creating movies'
 Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
 Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
 Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
 Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
 Movie.create(title: "The Wall: Pink Floyd", overview: "Pink Floyd – The Wall is a 1982 British live-action/animated psychological horror musical film directed by Alan Parker, based on Pink Floyd's 1979 album of the same name.", poster_url: "https://upload.wikimedia.org/wikipedia/en/9/94/Pink_Floyd_The_Wall.jpg", rating: 9.5)
+puts 'All movies created'
